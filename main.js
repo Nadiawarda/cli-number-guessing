05597df0f43b0750f0b6ce13e7154a48842bc172
Nadiawarda <144869724+Nadiawarda@users.noreply.github.com>
2024-03-27 15:15:06 +0500

    update2

--- main.js
+++ main.js
@@ -1,20 +1,33 @@
 #! /usr/bin/env node
 import inquirer from 'inquirer';
 import chalk from 'chalk';
-console.log("    ============= Welcom to Number Guessing Game ==========");
-const randomNumber = Math.floor(Math.random() * 10 + 1);
-const answer = await inquirer.prompt([
-    {
-        name: "userGuessedNumber",
-        type: "number",
-        message: "Please guess a number between 1-10: ",
-    },
-]);
-if (answer.userGuessedNumber === randomNumber) {
-    console.log(chalk.bold.yellowBright("Congratulations! You guessed the right number."));
+async function startGame() {
+    console.log("    ============= Welcome to Number Guessing Game ==========");
+    const randomNumber = Math.floor(Math.random() * 10 + 1);
+    const answer = await inquirer.prompt([
+        {
+            name: "userGuessedNumber",
+            type: "number",
+            message: "Please guess a number between 1-10: ",
+        },
+    ]);
+    if (answer.userGuessedNumber === randomNumber) {
+        console.log(chalk.bold.yellowBright("Congratulations! You guessed the right number."));
+    }
+    else {
+        console.log(chalk.bold.redBright("Sorry, you guessed the wrong number."));
+    }
+    const playAgain = await inquirer.prompt({
+        name: 'again',
+        type: 'confirm',
+        message: 'Do you want to play again?',
+        default: false
+    });
+    if (playAgain.again) {
+        startGame();
+    }
+    else {
+        console.log("      ======= Game Over ===========");
+    }
 }
-else {
-    console.log(chalk.bold.redBright("Sorry, You guessed the wrong number."));
-}
-;
-console.log("      ======= Game Over ===========");
+startGame();
