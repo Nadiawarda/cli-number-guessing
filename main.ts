5dac8f8e996e50bcb9a988eb38ceab973af6ca1b
Nadiawarda <144869724+Nadiawarda@users.noreply.github.com>
2024-03-28 09:59:23 +0500

    modified

--- main.ts
+++ main.ts
@@ -1,8 +1,6 @@
 #! /usr/bin/env node
-
 import inquirer from 'inquirer';
 import chalk from 'chalk';
-
 async function startGame() {
     console.log("    ============= Welcome to Number Guessing Game ==========");
     const randomNumber = Math.floor(Math.random() * 10 + 1);
@@ -19,19 +17,16 @@ async function startGame() {
     } else {
         console.log(chalk.bold.redBright("Sorry, you guessed the wrong number."));
     }
-
     const playAgain = await inquirer.prompt({
         name: 'again',
         type: 'confirm',
         message: 'Do you want to play again?',
         default: false
     });
-
     if (playAgain.again) {
         startGame();
     } else {
         console.log("      ======= Game Over ===========");
     }
 }
-
 startGame();
